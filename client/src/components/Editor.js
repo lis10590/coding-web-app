6dfa0d63c463130977fceedca1ec56a5339bb662
Lis Setgasy <lisset10590@gmail.com>
2022-12-14 00:28:13 +0200

    edited editor comp

--- client/src/components/Editor.js
+++ client/src/components/Editor.js
@@ -1,6 +1,8 @@
 import CodeMirror from "@uiw/react-codemirror";
+import { useState } from "react";
 
 const Editor = () => {
+  const [code, setCode] = useState("");
   return (
     <div>
       <CodeMirror height="20rem" width="40rem" />
